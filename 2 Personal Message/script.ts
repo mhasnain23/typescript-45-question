947929405be17185faa2c19b99b2a7329faad6f7
hasnaindev23 <hasnainop525@gmail.com>
2024-02-19 09:29:36 -0700

    nit

---
+++ 2 Personal Message/script.ts
@@ -0,0 +1,3 @@
+let pesonHasnain = "“Hello Hasnain, would you like to learn some Python today?”";
+
+console.log(pesonHasnain);
---
+++ 3 name cases/script.ts
@@ -0,0 +1,4 @@
+let person_Name:string = "Sir Zia";
+console.log(person_Name.toUpperCase());
+console.log(person_Name);
+console.log(person_Name.toLocaleLowerCase());
