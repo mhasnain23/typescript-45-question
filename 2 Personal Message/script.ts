48d7731e130dfadb6b53a157a1f683e7355b7726
hasnaindev23 <hasnainop525@gmail.com>
2024-02-26 15:27:19 -0700

    nit

--- 2 Personal Message/script.ts
+++ 2 Personal Message/script.ts
@@ -1,3 +1,3 @@
-let pesonHasnain = "“Hello Hasnain, would you like to learn some Python today?”";
+var pesonHasnain = "“Hello Hasnain, would you like to learn some Python today?”";
 
 console.log(pesonHasnain);
